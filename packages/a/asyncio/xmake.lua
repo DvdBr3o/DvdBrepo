e6e6ee7a22cb26f6ace427e27ccae231e907ec8e
DvdBr3o <3550954989@qq.com>
2023-05-01 23:59:45 +0800

    [New] Added package `netcan/asyncio`.

---
+++ packages/a/asyncio/xmake.lua
@@ -0,0 +1,12 @@
+package("asyncio")
+    set_homepage("https://github.com/netcan/asyncio")
+    set_description("asyncio is a c++20 library to write concurrent code using the async/await syntax.")
+
+    add_urls("https://github.com/netcan/asyncio.git")
+    add_versions("1.0.0", "8acad8fb12307e0de099f1e354f2eff30b1593aa")
+
+    add_deps("cmake")
+
+    on_install(function(package)
+        import("package.tools.cmake").install(package)
+    end)
