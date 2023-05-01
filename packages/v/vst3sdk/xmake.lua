05287fd1abca563f9d2825fa45b76bd106a944c8
DvdBr3o <3550954989@qq.com>
2023-05-02 01:25:25 +0800

    [New] Added package `vst3sdk`.

---
+++ packages/v/vst3sdk/xmake.lua
@@ -0,0 +1,16 @@
+package("vst3sdk")
+    set_homepage("https://github.com/steinbergmedia/vst3sdk")
+    set_description("VST 3 Plug-In SDK")
+
+    add_urls("https://github.com/steinbergmedia/vst3sdk.git")
+    add_versions("3.7.7", "358b72ee61bc67fb4592b0d492e0c6a1211ebf11")
+
+    add_deps("cmake")
+
+    add_configs("vstgui", { description = "Add VSTGUI Support", default = true, type = "boolean" })
+
+    on_install(function(package)
+        local configs = {}
+        table.insert(configs, "-DSMTG_ADD_VST3_PLUGINS_SAMPLES=" .. package:config("vstgui") and "ON" or "OFF")
+        import("package.tools.cmake").install(package, configs)
+    end)
