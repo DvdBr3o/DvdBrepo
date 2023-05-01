dad20ec758dcfc3efb3a5e9fffb6d8743cdb720b
DvdBr3o <3550954989@qq.com>
2023-05-01 22:50:13 +0800

    [New] Added package `nanovg_bgfx`.

--- packages/n/nanovg_bgfx/xmake.lua
+++ packages/n/nanovg_bgfx/xmake.lua
@@ -1,6 +1,5 @@
 package("nanovg_bgfx")
     set_description("The nanovg utilities under bgfx.")
-
     set_urls("https://github.com/DvdBr3o/nanovg_bgfx.git")
     on_install(function(package)
         local configs = {}
